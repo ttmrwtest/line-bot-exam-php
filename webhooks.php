b12b320c54847b39dd231d5382b2537cf33dc60b
ttmrwtest <40158397+ttmrwtest@users.noreply.github.com>
2018-06-11 11:57:19 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'xW6uoHrAFv8eMuH3Vj3EkfLk2V/LfZJiSkJdQ67oCF3JzJsM4e3Gp/RRXDsnrGUckM/o7n0ReU4d3E/tHjVkZFgZBhcEObv2rAXyES2y5AGrDbXa8BW741GpbQyF9wQhr/IewptCr18ifH3jMcGerAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
